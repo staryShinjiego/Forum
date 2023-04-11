94074ddb60e65a5e1b242a5186adc8fb27efecf2
staryShinjiego <102579901+staryShinjiego@users.noreply.github.com>
2023-04-11 13:03:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum Internetowe</title>
+    <link rel="stylesheet" href="styl_index.css">
+</head>
+<body>
+    <header>
+        <h1>Forum Internetowe<h1>
+    </header>
+    <br>
+    <main>
+        <h3>Logowanie:<h3>
+        <div class="form__group field">
+        <label for="name" class="form__label">Login:</label>
+        <div><input type="input" class="form__field" placeholder="         przykladowy_login" name="login" id='login' required />
+        </div>
+        <div class="form__group field">
+        <label for="name" class="form__label">Hasło:</label>
+        <div><input type="input" class="form__field" placeholder="                   ********" name="haslo" id='haslo' required />
+        </div>
+        <h4>Nie posiadasz konta?
+        Zarejestruj się tutaj ↓ </h4>
+        <form action="">
+        <input type="submit" value="Zarejestruj" />
+        </form>
+    </main>
+</body>
+</html>
